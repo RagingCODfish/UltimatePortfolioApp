d9befee1e1ae3f878c82b506c9fd9a53f4d9edc2
RagingCODfish <zachary.uptin@me.com>
2022-10-14 21:58:04 +1000

    Initial Commit

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Zach Uptin on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
