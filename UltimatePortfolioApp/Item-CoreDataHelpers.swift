2113f4281164b1cbb2ba464efa3ebf97a6949929
RagingCODfish <zachary.uptin@me.com>
2022-10-24 12:59:12 +1000

    Added Core Data Helper Files Cleaned up call sites in views

---
+++ UltimatePortfolioApp/Item-CoreDataHelpers.swift
@@ -0,0 +1,37 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolioApp
+//
+//  Created by Zach Uptin on 24/10/2022.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+    
+    var itemDetail: String {
+        detail ?? ""
+    }
+    
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example Item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+    
+}
