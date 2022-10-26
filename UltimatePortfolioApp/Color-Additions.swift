4bb17a52977e39e3dd93dd8a54285b742351c06c
RagingCODfish <zachary.uptin@me.com>
2022-10-26 23:56:25 +1000

    Creating the Home view

---
+++ UltimatePortfolioApp/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolioApp
+//
+//  Created by Zach Uptin on 26/10/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
