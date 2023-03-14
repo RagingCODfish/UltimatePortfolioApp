e531a658a1913a2d9e592492b9bb14e4ee2049d6
RagingCODfish <zachary.uptin@me.com>
2023-03-14 22:00:21 +1000

    Fixed Bugs. Version currently working

--- UltimatePortfolioApp/Bundle-Decodable.swift
+++ UltimatePortfolioApp/Bundle-Decodable.swift
@@ -33,9 +33,9 @@ extension Bundle {
 		} catch DecodingError.typeMismatch(_, let context) {
 			fatalError("Failed to decode \(file) due to type mismatch - \(context.debugDescription)")
 		} catch DecodingError.valueNotFound(let type, let context) {
-			fatalError("Failed to deoce \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+			fatalError("Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
 		} catch DecodingError.dataCorrupted(_) {
-			fatalError("Failed to deoce \(file) from bundle because it appears to be invalid JSON.")
+			fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
 		} catch {
 			fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
 		}
--- UltimatePortfolioApp/UltimatePortfolioAppApp.swift
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -23,8 +23,8 @@ struct UltimatePortfolioAppApp: App {
                 .environmentObject(dataController)
                 .onReceive(
 					// Automatically save when we detect that we are no longer the foreground app.
-					// Use this rather then the scene pahase API so we can port to macOS,
-					// where scene phase won't detect our app losing docus as of macOS 11.1.
+					// Use this rather then the scene phase API so we can port to macOS,
+					// where scene phase won't detect our app losing focus as of macOS 11.1.
 					NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
 						   perform: save)
         }
