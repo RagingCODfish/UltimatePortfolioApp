7ad901f7c9c4221093ff5753547a28f055a52b96
Zach Uptin <zachary.uptin@me.com>
2023-03-19 21:58:12 +1000

    Bringing MVVM into our SwiftUI project, Part 1

---
+++ UltimatePortfolioApp/Activities/Projects/ProjectsView.swift
@@ -0,0 +1,116 @@
+//
+//  ProjectsView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Zach Uptin on 21/10/2022.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+	static let openTag: String? = "Open"
+	static let closedTag: String? = "Closed"
+
+    @State private var showingSortOrder = false
+    @StateObject var viewModel: ViewModel
+
+	var projectsList: some View {
+		List {
+            ForEach(viewModel.projects) { project in
+				Section(header: ProjectHeaderView(project: project)) {
+                    ForEach(project.projectItems(using: viewModel.sortOrder)) { item in
+						ItemRowView(project: project, item: item)
+					}
+					.onDelete { offsets in
+                        viewModel.delete(offsets, from: project)
+					}
+
+                    if viewModel.showClosedProjects == false {
+						Button {
+                            withAnimation {
+                                viewModel.addItem(to: project)
+                            }
+						} label: {
+							Label("Add New Item", systemImage: "plus")
+						}
+					}
+				}
+			}
+		}
+		.listStyle(InsetListStyle())
+	}
+
+	var addProjectToolbarItem: some ToolbarContent {
+		ToolbarItem(placement: .navigationBarTrailing) {
+            if viewModel.showClosedProjects == false {
+                Button {
+                    withAnimation {
+                        viewModel.addProject()
+                    }
+                } label: {
+					// In iOS 14.3 VoiceOver has a glitch that reads the label "Add Project"
+					// as "Add" no matter what accessibility label
+					// We give this toolbar button when using a Label.
+					// As a result, when VoiceOver is running, we use a text view for the button instead,
+					// forcing a correct reading without losing the original layout.
+					if UIAccessibility.isVoiceOverRunning {
+						Text("Add Project")
+					} else {
+						Label("Add Project", systemImage: "plus")
+					}
+				}
+			}
+		}
+	}
+
+	var sortOrderToolbarItem: some ToolbarContent {
+		ToolbarItem(placement: .navigationBarLeading) {
+			Button {
+				showingSortOrder.toggle()
+			} label: {
+				Label("Sort", systemImage: "arrow.up.arrow.down")
+			}
+		}
+	}
+
+	var body: some View {
+		NavigationView {
+			Group {
+                if viewModel.projects.isEmpty {
+					Text("There's nothing here right now")
+						.foregroundColor(.secondary)
+				} else {
+					projectsList
+				}
+			}
+            .navigationTitle(viewModel.showClosedProjects ? "Closed Projects" : "Open Projects")
+			.toolbar {
+				addProjectToolbarItem
+				sortOrderToolbarItem
+			}
+			.actionSheet(isPresented: $showingSortOrder) {
+				ActionSheet(title: Text("Sort Items"), message: nil, buttons: [
+                    .default(Text("Optimized")) { viewModel.sortOrder = .optimized},
+                    .default(Text("Creation Date")) { viewModel.sortOrder = .creationDate},
+                    .default(Text("Title")) { viewModel.sortOrder = .title}
+
+				])
+			}
+
+			SelectSomethingView()
+		}
+	}
+
+    init(dataController: DataController, showClosedProjects: Bool) {
+        let viewModel = ViewModel(dataController: dataController, showClosedProjects: showClosedProjects)
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+	static var dataController = DataController.preview
+
+	static var previews: some View {
+        ProjectsView(dataController: DataController.preview, showClosedProjects: false)
+	}
+}
