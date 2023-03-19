9e3fa0115df6c08b143e8042e501dbb893a37287
Zach Uptin <zachary.uptin@me.com>
2023-03-19 22:54:27 +1000

    Bringing MVVM into our SwiftUI project, part 2

---
+++ UltimatePortfolioApp/Activities/Projects/ItemRowView.swift
@@ -0,0 +1,38 @@
+//
+//  ItemRowView.swift
+//  UltimatePortfolioApp
+//
+//  Created by Zach Uptin on 24/10/2022.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @StateObject var viewModel: ViewModel
+    @ObservedObject var item: Item
+
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Label {
+                Text(item.itemTitle)
+            } icon: {
+                Image(systemName: viewModel.icon)
+                    .foregroundColor(viewModel.color.map { Color($0) } ?? .clear)
+            }
+        }
+        .accessibilityLabel(viewModel.label)
+    }
+
+    init(project: Project, item: Item) {
+        let viewModel = ViewModel(project: project, item: item)
+        _viewModel = StateObject(wrappedValue: viewModel)
+
+        self.item = item
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(project: Project.example, item: Item.example)
+    }
+}
